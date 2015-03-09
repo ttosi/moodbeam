913c3d4417b06f77e3b80dad848e19aba99d5451
ttosi <ttosi519@gmail.com>
2015-03-09 11:02:02 -0700

    Update web client

--- esp8266/sta_mode.lua
+++ esp8266/sta_mode.lua
@@ -15,29 +15,29 @@ conn:on("connection", function(conn)
 	conn:send(u..":"..p)
 end)
 
+tmr.alarm(1, 300000, 1, function()
+	conn:send(..u..":keepalive")
+end)
+
 conn:on("receive", function(conn, data)
-	cmd = {}
+	params = {}
 	for param in string.gmatch(data, "(%w+)") do
-		table.insert(cmd, param)
+		table.insert(params, param)
 	end
 	
-	if cmd[1] ~= nil then
-		if cmd[1] == "showcolor" then
-			writeSerialCommand({ 0xF0, cmd[2], cmd[3], cmd[4] })
-		elseif cmd[1] == "setbrightness" then
-			writeSerialCommand({ 0xF1, cmd[2] })
-		elseif cmd[1] == "showpreset" then
-			writeSerialCommand({ 0xF2, cmd[2] })
-		elseif cmd[1] == "definepreset" then
-			writeSerialCommand({ 0xF3, cmd[2], cmd[3], cmd[4], cmd[5] })
-		elseif cmd[1] == "alternatecolors" then
-			writeSerialCommand({ 0xF4, cmd[2], cmd[3] })
-		elseif cmd[1] == "twocolor" then
-			writeSerialCommand({ 0xF5, cmd[2], cmd[3] })
-		elseif cmd[1] == "flashcolor" then
-			writeSerialCommand({ 0xF6, cmd[2] })
-		elseif cmd[1] == "showrainbow" then
-			writeSerialCommand({ 0xF7, cmd[2] })
+	if params[1] ~= nil then
+		if params[1] == "showcolor" then
+			writeSerialCommand({ 0xF0, params[2], params[3], params[4] })
+		elseif params[1] == "setbrightness" then
+			writeSerialCommand({ 0xF1, params[2] })
+		elseif params[1] == "alternatecolors" then
+			writeSerialCommand({ 0xF4, params[2], params[3], params[4], params[5], params[6], params[7] })
+		elseif params[1] == "twocolor" then
+			writeSerialCommand({ 0xF5, params[2], params[3], params[4], params[5], params[6], params[7] })
+		elseif params[1] == "flashcolor" then
+			writeSerialCommand({ 0xF6, params[2], params[3], params[4] })
+		elseif params[1] == "showrainbow" then
+			writeSerialCommand({ 0xF7, params[2] })
 		end
 	end
 end)
